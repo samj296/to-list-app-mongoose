b08e4d96dc43a28cbf6f4f358dac42944b4b6cf6
Sam J <sam_joseph@live.com>
2026-02-02 17:00:17 -0800

    Working on the listView created the function to render the todo

---
+++ public/assets/script/carouselView.js
@@ -0,0 +1,26 @@
+    /*
+    This is the example of carousel(bootstrap) 
+    I tried this and now copied here so that 
+    I can inject it whith js when Carousal view is selected
+    */
+
+    
+    // <div id = "carouselTodo" class = "carousel slide">
+    //                 <div class = "carousel-inner">
+    //                     <div class = "carousel-item active text-center">
+    //                         <p class="d-block w-100">ghsdfjhgsdkfjsdhfg</p>
+    //                     </div>
+    //                     <div class = "carousel-item active text-center justify-content-center">
+    //                         <p class="d-block w-100">dskfjgwsednfbcgu</p>
+    //                     </div>
+    //                 </div>
+    //                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselTodo" data-bs-slide="prev">
+    //                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    //                     <span class="visually-hidden">Previous</span>
+    //                 </button>
+    //                 <button class="carousel-control-next" type="button" data-bs-target="#carouselTodo" data-bs-slide="next">
+    //                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    //                     <span class="visually-hidden">Next</span>
+    //                 </button>
+
+    // </div>
